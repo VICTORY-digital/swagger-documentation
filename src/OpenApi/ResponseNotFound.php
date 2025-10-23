b87d0a2fb30a6926e0792fb1b4383e6ad7b5b331
loveabdrain <otchepenec49@gmail.com>
2025-10-23 12:57:56 +0500

    add not found response

---
+++ src/OpenApi/ResponseNotFound.php
@@ -0,0 +1,21 @@
+<?php
+
+declare(strict_types=1);
+
+namespace VictoryGroup\SwaggerDocumentation\OpenApi;
+
+use Attribute;
+use OpenApi\Attributes\Response;
+use Symfony\Component\HttpFoundation\Response as SymfonyResponse;
+
+#[Attribute(Attribute::TARGET_CLASS | Attribute::TARGET_METHOD | Attribute::IS_REPEATABLE)]
+final class ResponseNotFound extends Response
+{
+    public function __construct()
+    {
+        parent::__construct(
+            response: SymfonyResponse::HTTP_NOT_FOUND,
+            description: 'Не найдено',
+        );
+    }
+}
